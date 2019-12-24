48fadb313a2967bd990c23bc5aea9a3527d50448
hacker <lkha.stolyarov@bk.ru>
2019-12-24 16:40:56 +0900

    fix error 2.0

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x5_fo9efv%&+4crz&(pn^nls8r_(le!cfkwy4ot1@e*cclfx^&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['aleksey56984.pythonanywhere.com']
 
