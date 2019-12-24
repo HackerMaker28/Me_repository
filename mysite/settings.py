4548409f54aedcaf4cdf15d2e6cc928434148a9b
hacker <lkha.stolyarov@bk.ru>
2019-12-24 16:35:46 +0900

    fix errir

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x5_fo9efv%&+4crz&(pn^nls8r_(le!cfkwy4ot1@e*cclfx^&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['aleksey28rus.pythonanywhere.com']
+ALLOWED_HOSTS = ['aleksey56984.pythonanywhere.com']
 
 
 # Application definition
